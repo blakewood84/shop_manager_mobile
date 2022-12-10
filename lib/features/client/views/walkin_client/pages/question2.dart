3a245e779385ae13292e6e36b4a75cc5de9229ea
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-12-10 14:20:16 -0800

    added question 2

--- lib/features/client/views/walkin_client/pages/question2.dart
+++ lib/features/client/views/walkin_client/pages/question2.dart
@@ -11,31 +11,95 @@ class Question2Page extends ConsumerWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Question2'),
+          const Text(
+            'Are you an Adult or a Minor?',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 20.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                onPressed: () {
-                  ref.read(pageControllerProvider).previousPage(
-                        duration: const Duration(milliseconds: 400),
-                        curve: Curves.easeInOut,
-                      );
-                },
-                child: Text('Back Page'),
+              SizedBox(
+                height: 120,
+                width: 120,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrangeAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(360),
+                    ),
+                  ),
+                  onPressed: () {
+                    ref.read(question2AnswerProvider.notifier).state = AgeType.adult;
+                    ref
+                        .read(pageControllerProvider)
+                        .nextPage(duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
+                  },
+                  child: const Text(
+                    'Adult',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ),
-              const SizedBox(width: 40),
-              ElevatedButton(
-                onPressed: () {
-                  ref.read(pageControllerProvider).nextPage(
-                        duration: const Duration(milliseconds: 400),
-                        curve: Curves.easeInOut,
-                      );
-                },
-                child: Text('Next Page'),
+              const SizedBox(width: 20.0),
+              SizedBox(
+                height: 120,
+                width: 120,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.greenAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(360),
+                    ),
+                  ),
+                  onPressed: () {
+                    ref.read(question1AnswerProvider.notifier).state = WorkType.piercing;
+                    ref
+                        .read(pageControllerProvider)
+                        .nextPage(duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
+                  },
+                  child: const Text(
+                    'Minor',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
+          const SizedBox(height: 20.0),
+          TextButton(
+            onPressed: () {
+              ref
+                  .read(pageControllerProvider)
+                  .previousPage(duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Icon(
+                  Icons.arrow_back,
+                  size: 40,
+                ),
+                SizedBox(width: 5.0),
+                Text(
+                  'Back',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
